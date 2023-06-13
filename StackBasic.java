2b39b4fbc5ff4fe939d72ed0d65c7fcfff5f536a
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2023-06-13 19:49:44 +0800

    Done making the first condition

--- StackBasic.java
+++ StackBasic.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 public class StackBasic {
-private int maxSize; private int top; private int[] stackArray; // constructor public StackBasic(int size) {
-maxSize = size; stackArray = new int[maxSize]; top = -1; // initially stack is empty
+private int maxSize; private int top; private int[] stackArray; 
+
+// constructor 
+public StackBasic(int size) {
+    maxSize = size;
+    stackArray = new int[maxSize];
+    top = -1; // initially stack is empty
 }
-// Add element to top of stack public void push(int value) { //…….. Code here
+// Add element to top of stack public void push(int value)
+public void push(int value) {
+    if (top == maxSize - 1) {
+        System.out.println("The stack is full, element cannot be push");
+    } 
+    else {
+        stackArray[++top] = value;
+        System.out.println("The element is pushed, " + value + ".");
+    }
 }
+
 // Remove element from top of stack public void pop() {
 //.. Code here
 }
